e4a9fac753a59c0ee689c65b3f6648287bfa9010
brittanikinzalow <brittanikinzalow@gmail.com>
2022-09-23 13:31:59 -0400

    update

--- game_show_app_v1/app.js
+++ game_show_app_v1/app.js
@@ -1,68 +1,75 @@
 const qwerty = document.querySelector('#qwerty');
 const startButton = document.querySelector('.btn__reset');
-const phrase = [
-    { question:'Who is Haunting Adeline?',
-        answer: 'Zade'
-    },
-
-    { question:"What is Penellaphe Da'Neer?",
-    answer: 'Goddess'
-    },
-
-    { question:'Who is the king in the Kingdom of the Wicked?',
-    answer: 'Wrath'
-    },
-
-    { question:'Who is the bride of the shadow king?',
-    answer: 'Princess Faraine'
-    },
-
-    { question:'What type off creature is Aria from Crown of Chaos series?',
-    answer: 'Pheonix'
-    },
-
-    { question:'What did the woman from The Lost Apothecary sell?',
-    answer: 'Pioson'
-    },
-
-    { question:'Who is the author of These Hollow Vows?',
-    answer: 'Lexi Ryan'
-    },
-
-    { question:'Whos is the Cruel Prince?',
-    answer: 'Carden'
-    },
-
-    { question:'Who is the author of Hooked?',
-    answer: 'Emily McIntire'
-    },
-
-    { question:'What is the name of Eragons dragon?',
-    answer: 'Saphira'
-    },
+const book = [ 
+    'from blood and ash',
+    'hooked',
+    'cruel prince',
+    'ashes of chaos',
 ];
+
+let missed = 0;
+
+
+
+
+
+
 
 //array random phrase
 function getRandomPhraseAsArrray(arr) {
-    const question = document.querySelector('.question');
-    const getRandomNumber = Math.ceil(math.random() * 10);
-    const randomArray = arr[getRandomNumber];
-    const getRandomQuestion = randomArray.question;
-    const getRandomAnswer = randomArray.answer;
+    const book = document.querySelector('.book');
+    const randomNumber = Math.floor(math.random() * 4);
+    const randomArr = arr[randomNumber];
 
     return getRandomAnswer;
+    phrase.innerText = `${randomStrings}`;
 };
+
+
+
+
+
+
+
+
 
 //add letters of string to the display
 
 function addPhraseToDisplay(arr) {
-    const phraseUl = document.querySelector('#phrase ul');
+    const phraseUl = document.querySelector('#book ul');
     const randomArray = getRandomPhraseAsArrray(arr);
-}
+    const splitPhrase = randomArray.split('');
+
+    for (const i = 0; phraseSplit.length; i++){
+        const li = document.createElement('li');
+        const HTML = `${phraseSplit[i]}`;
+
+        if (phraseSplit[i] != ''){
+            li.classList.add('letter');
+        } else {
+            li.classList.add('space');
+        }
+
+        phraseUL.appendChild(li);
+    }
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //check if a letter is in the phrase
 
-const checkLetter = button => {}
+function checkLetter(button) { }
 
 //check if the game has been won or lost
 
