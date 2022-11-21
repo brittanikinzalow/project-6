e042e032b46a358553bd406990214f7fef3463ed
brittanikinzalow <brittanikinzalow@gmail.com>
2022-11-21 00:23:22 -0500

    update

--- game_show_app_v1/app.js
+++ game_show_app_v1/app.js
@@ -1,58 +1,31 @@
 const qwerty = document.querySelector('#qwerty');
 const startButton = document.querySelector('.btn__reset');
-const book = [ 
-    'from blood and ash',
-    'hooked',
-    'cruel prince',
-    'ashes of chaos',
-];
+const phrase = document.querySelector('phrase');
+
+
+
+function stub() {};
+
 
 let missed = 0;
 
 
+let phrases = [
+    'merry christmas and happy new year',
+    'have a blessed day',
+    'catch me if you can',
+    'knock knock whose there',
+    'see you later alligator'
+];
 
 
 
-
-
-//array random phrase
-function getRandomPhraseAsArrray(arr) {
-    const book = document.querySelector('.book');
-    const randomNumber = Math.floor(math.random() * 4);
-    const randomArr = arr[randomNumber];
-
-    return getRandomAnswer;
-    phrase.innerText = `${randomStrings}`;
+let getRandomPhraseAsArray = arr => {
+    arr [Math.floor(Math.random() * phrases.length)];
 };
 
 
-
-
-
-
-
-
-
-//add letters of string to the display
-
-function addPhraseToDisplay(arr) {
-    const phraseUl = document.querySelector('#book ul');
-    const randomArray = getRandomPhraseAsArrray(arr);
-    const splitPhrase = randomArray.split('');
-
-    for (const i = 0; phraseSplit.length; i++){
-        const li = document.createElement('li');
-        const HTML = `${phraseSplit[i]}`;
-
-        if (phraseSplit[i] != ''){
-            li.classList.add('letter');
-        } else {
-            li.classList.add('space');
-        }
-
-        phraseUL.appendChild(li);
-    }
-        };
+let selectedPhrase = phrases[Math.floor(Math.random() * phrases.length)];
 
 
 
@@ -62,27 +35,12 @@ function addPhraseToDisplay(arr) {
 
 
 
+const addPhraseToDisplay = arr => {
 
+}
 
-
-
-
-//check if a letter is in the phrase
-
-function checkLetter(button) { }
-
-//check if the game has been won or lost
-
-const checkWin = () => {}
-
-//listen to see if the start game button has been pressed
+//event listener for start game//
 
 startButton.addEventListener('click', () => {
-    const overLay =document.querySelector('#overlay');
-    overLay.style.display = 'none';
+    overlay.style.display = 'none';
 });
-
-//listen for onscreen keyboard to be clicked
-
-qwerty.addEventListener('click', e => {});
-
